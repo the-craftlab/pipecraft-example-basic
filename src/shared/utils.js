7bf7c3e2c98c7874dd6ecc1d245b1a3b3a29ded5
pipecraft-ci <ci@thecraftlab.dev>
2026-09-06 22:51:59 -0400

    chore: pipecraft example baseline

---
+++ src/shared/utils.js
@@ -0,0 +1,34 @@
+/**
+ * Shared Domain - FAKE CODE
+ *
+ * This is completely fake code that does nothing.
+ * Used to test PipeCraft's domain detection and testing workflows.
+ */
+
+console.log('Shared domain loaded (fake)')
+
+// Fake utility functions
+function fakeHelper() {
+  return 'fake helper'
+}
+
+function fakeValidator() {
+  return true
+}
+
+function fakeFormatter() {
+  return 'fake formatted'
+}
+
+// Fake constants
+const FAKE_CONSTANTS = {
+  API_URL: 'https://fake-api.com',
+  VERSION: '1.0.0-fake'
+}
+
+module.exports = {
+  fakeHelper,
+  fakeValidator,
+  fakeFormatter,
+  FAKE_CONSTANTS
+}
